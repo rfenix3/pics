e84568b44127fb519f4e367c4e6f620769bb5ac6
rfenix3 <richard.fenix@gmail.com>
2019-06-13 08:44:39 -0700

    Convert App into a class and create onSearchSubmit callback function.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container">
-      <header className="App">
-        <SearchBar />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <header className="App">
+          <SearchBar onSubmit={this.onSearchSubmit}/>
+       </header>
+     </div>
+   );
+  }
 }
 
 export default App;
