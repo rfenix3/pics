ffca8a5334a77747a197d0ef0c67efa391a51760
rfenix3 <richard.fenix@gmail.com>
2019-06-13 07:34:47 -0700

    Refactor Searchbar element into a controlled react element.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,20 +2,17 @@ import React from 'react';
 
 
 class SearchBar extends React.Component {
+  state = {term: ''};
   
-  // onInputChange() is a callback function
-  // the property 'onChange' is a special property name. Others are 'onClick' and 'onSubmit'.
-  // callback naming convention. 'on' then the name of the element 'Input', then the event we are watching 'Change'.
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input type="text" 
+              value={this.state.term} 
+              onChange={(e) => this.setState({term: e.target.value})} />
           </div>
         </form>
       </div>
