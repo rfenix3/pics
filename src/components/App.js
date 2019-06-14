053735f33391f61b1ad52d0e488e8b7182d6ba4e
rfenix3 <richard.fenix@gmail.com>
2019-06-14 11:19:36 -0700

    Install axios and babel runtime (npm add @babel/runtime) and add API search.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID b11b27b5346cfdf271be1228d9f5b74a47e7b2492d0705cdef895743825ba8a5'
+      }
+    });
   }
 
   render() {
