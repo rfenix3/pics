74ab087105c2e40e9df9a02b3b2a3219bc070e73
rfenix3 <richard.fenix@gmail.com>
2019-06-27 20:15:01 -0700

    Create grids to format the display of images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,20 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
   console.log(props.images);
   const images = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.regular} alt={image.alt_description} />
+    return < ImageCard key={image.id} image={image} />
   })
+
+  // An alterative way is to destructure image
+  // const images = props.images.map(({id, urls, description}) => {
+  //   return <img key={id} src={urls.regular} alt={description} />
+  // })
+
   return (
-  <div>{images}</div>
+  <div className="image-list">{images}</div>
   )
 }
 
